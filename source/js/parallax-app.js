2e385c065e26d1151d29343a7a7a1d5be904c18f
Nikaytos <nikaytos@gmail.com>
2024-05-17 01:08:24 +0300

    6 lab fix add comments

--- source/js/parallax-app.js
+++ source/js/parallax-app.js
@@ -2,8 +2,9 @@ const textElement = document.getElementById("parallaxText");
 
 window.addEventListener("scroll", function () {
   const currentScrollPosition = window.scrollY;
-
-  // Змінено з transform на opacity
+  /*
+   * Паралакс ефект
+   */
   textElement.style.opacity = `${
     1 - currentScrollPosition / (window.innerHeight * 0.5)
   }`;
